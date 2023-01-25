4e16f6e3c7fae31cb77a56c2f142e5518f8a7f0f
Group018 <Group018@infoway.com>
2023-01-25 12:09:13 +0530

    added

---
+++ src/test/java/group018/testmain.java
@@ -0,0 +1,14 @@
+package group018;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testmain {
+
+	@Test
+	public void testInputIsEven(){
+	assertTrue(Main.checkIfInputIsAnEvenNumber(21)); //
+	
+	}
+	}
